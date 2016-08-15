38577a666bb16f537cc1464368a92d5bd916bfb4
Zhao Li <zhao.li@uhalumni.org>
2016-08-14 22:45:31 -1000

    post to IFTTT maker channel

--- index.js
+++ index.js
@@ -1,106 +1,13 @@
 /**
- * This is a sample Lambda function that sends an Email on click of a
- * button. It creates a SNS topic, subscribes an endpoint (EMAIL)
- * to the topic and publishes to the topic.
+ * This is a simple Lambda function that triggers the IFTTT maker channel on click of a
+ * button.
  *
  * Follow these steps to complete the configuration of your function:
  *
- * 1. Update the EMAIL variable with your email address.
- * 2. Enter a name for your execution role in the "Role name" field.
- *    Your function's execution role needs specific permissions for SNS operations
- *    to send an email. We have pre-selected the "AWS IoT Button permissions"
- *    policy template that will automatically add these permissions.
+ * 1. Update with IFTTT maker key
+ * 2. Update with IFTTT maker event
  */
-
-const EMAIL = 'example@email.com';  // TODO change me
-
-const AWS = require('aws-sdk');
-const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
-
-function findExistingSubscription(topicArn, nextToken, cb) {
-    const params = {
-        TopicArn: topicArn,
-        NextToken: nextToken || null,
-    };
-    SNS.listSubscriptionsByTopic(params, (err, data) => {
-        if (err) {
-            console.log('Error listing subscriptions.', err);
-            cb(err);
-            return;
-        }
-        const subscription = data.Subscriptions.filter((sub) => sub.Protocol === 'email' && sub.Endpoint === EMAIL)[0];
-        if (!subscription) {
-            if (!data.NextToken) {
-                cb(null, null); // indicate that no subscription was found
-            } else {
-                findExistingSubscription(topicArn, data.NextToken, cb); // iterate over next token
-            }
-        } else {
-            cb(null, subscription); // a subscription was found
-        }
-    });
-}
-
-/**
- * Subscribe the specified EMAIL to a topic.
- */
-function createSubscription(topicArn, cb) {
-    // check to see if a subscription already exists
-    findExistingSubscription(topicArn, null, (err, res) => {
-        if (err) {
-            console.log('Error finding existing subscription.', err);
-            cb(err);
-            return;
-        }
-        if (!res) {
-            // no subscription, create one
-            const params = {
-                Protocol: 'email',
-                TopicArn: topicArn,
-                Endpoint: EMAIL,
-            };
-            SNS.subscribe(params, (err, data) => {
-                if (err) {
-                    console.log('Error setting up email subscription.', err);
-                    cb(err);
-                    return;
-                }
-                // subscription complete
-                console.log(`Subscribed ${EMAIL} to ${topicArn}.`);
-                cb(null, topicArn);
-            });
-        } else {
-            // subscription already exists, continue
-            cb(null, topicArn);
-        }
-    });
-}
-
-/**
- * Create a topic.
- */
-function createTopic(topicName, cb) {
-    SNS.createTopic({ Name: topicName }, (err, data) => {
-        if (err) {
-            console.log('Creating topic failed.', err);
-            cb(err);
-            return;
-        }
-        const topicArn = data.TopicArn;
-        console.log(`Created topic: ${topicArn}`);
-        console.log('Creating subscriptions.');
-        createSubscription(topicArn, (err, data) => {
-            if (err) {
-                cb(err);
-                return;
-            }
-            // everything is good
-            console.log('Topic setup complete.');
-            cb(null, topicArn);
-        });
-    });
-}
-
+ 
 /**
  * The following JSON template shows what is sent as the payload:
 {
@@ -116,22 +23,34 @@ function createTopic(topicName, cb) {
  * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
  */
 exports.handler = (event, context, callback) => {
-    console.log('Received event:', event.clickType);
+  var https    = require('https'),
+      url      = require('url'),
+      endpoint = url.parse(
+        'https://maker.ifttt.com/trigger/my_event/with/key/my_key'
+      ); // update my_event and my_key
 
-    // create/get topic
-    createTopic('aws-iot-button-sns-topic', (err, topicArn) => {
-        if (err) {
-            callback(err);
-            return;
-        }
-        console.log(`Publishing to topic ${topicArn}`);
-        // publish message
-        const params = {
-            Message: `${event.serialNumber} -- processed by Lambda\nBattery voltage: ${event.batteryVoltage}`,
-            Subject: `Hello from your IoT Button ${event.serialNumber}: ${event.clickType}`,
-            TopicArn: topicArn
-        };
-        // result will go to function callback
-        SNS.publish(params, callback);
-    });
+  console.log('Received event:', event.clickType);
+  console.log('start request to ' + endpoint.href);
+
+  var jsonObject = JSON.stringify(event);
+  var options = {
+    host: endpoint.host,
+    port: 443,
+    path: endpoint.path,
+    method: 'POST',
+  };
+
+  var post = https.request(options, function(res) {
+    console.log("Got response: " + res.statusCode);
+    context.succeed();
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    context.done(null, 'FAILURE');
+  });
+
+  post.write(jsonObject);
+  post.end();
+
+  console.log('end request to ' + endpoint.href);
 };
+
